701869267fd255c73050901df5bec846abfd74be
Shiva181006 <shivakasaudhan817@gmail.com>
2026-06-22 17:59:28 +0530

    Add GitHub users API integration and dynamic profile rendering

---
+++ Git API Call Project/index.js
@@ -0,0 +1,40 @@
+async function github(){
+
+
+  try{
+
+    const response = await fetch("https://api.github.com/users");
+  
+    if(!response.ok){
+      throw new Error ("data is not present");
+    }
+    const data = await response.json();
+    
+    const parent = document.getElementById("first");
+  
+    for(let user of data){
+  
+      const element = document.createElement("div");
+      element.classList.add("user");
+  
+      const image = document.createElement("img");
+      image.src = user.avatar_url;
+  
+      const userName = document.createElement("h2");
+      userName.textContent = user.login;
+  
+      const anchor = document.createElement("a");
+      anchor.href = user.html_url;
+      anchor.textContent = "Visit Profile";
+  
+      element.append(image,userName,anchor);
+      parent.append(element);
+  
+    }
+  }
+  catch(err){
+    console.log("Error :",err);
+  }
+  }
+
+github()
