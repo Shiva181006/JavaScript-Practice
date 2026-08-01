2a22a16cae4dc90329680c2e5811fd00ed9eea04
Shiva181006 <shivakasaudhan817@gmail.com>
2026-08-01 21:16:07 +0530

    Solve the leetcode question in which we have to find the day of the year

---
+++ DSA/String/1154_Day_Of_Year.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} date
+ * @return {number}
+ */
+var dayOfYear = function(date) {
+
+    let [year, month, day] = date.split("-").map(Number);
+
+    let days = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if (
+        year % 400 === 0 ||
+        (year % 4 === 0 && year % 100 !== 0)
+    ) {
+        days[1] = 29;
+    }
+
+    let ans = day;
+
+    for (let i = 0; i < month - 1; i++) {
+        ans += days[i];
+    }
+
+    return ans;
+};
+
+console.log(dayOfYear());
